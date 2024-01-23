0e750566d2149f6e993d9db7155003c653adc035
Guido Mantegna <gmantegna@elcallao.com>
2024-01-23 07:59:55 -0300

    Fullfill the 'heart' when a reply is liked

--- src/routes/Request.tsx
+++ src/routes/Request.tsx
@@ -1,8 +1,9 @@
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import AskForm from 'components/AskForm'
 import Ask from 'components/Ask'
 import useRequests from '../customHooks/useRequests'
-import { GoCommentDiscussion, GoQuestion, GoHeart } from 'react-icons/go'
+import { GoCommentDiscussion, GoQuestion, GoHeart, GoHeartFill } from 'react-icons/go'
 import { getFormattedDate } from 'utils'
 import { useContext } from 'react'
 import { AuthContext } from 'context'
@@ -12,6 +13,7 @@ const Request: React.FC = () => {
   const { id } = useParams<{ id: string }>()
   const { asks, loading, error } = useRequests(id)
   const user = useContext(AuthContext)
+  // const [isLiked, setIsLiked] = useState(false)
   const handleLike = async (replyID: string) => {
     try {
       await axios.patch(`http://localhost:1234/api/v1/replies/${replyID}`, {
@@ -21,6 +23,7 @@ const Request: React.FC = () => {
       console.log(error)
     }
   }
+  const isLiked = asks?.[0].replies[0].likes.some((like) => like._id === user?._id)
   return (
     <>
       {loading || !asks ? (
@@ -56,7 +59,7 @@ const Request: React.FC = () => {
             </div>
             <div className="flex justify-end items-center gap-2 text-sm">
               <button className='flex gap-1 pr-2' onClick={() => handleLike(reply._id)}>
-                <GoHeart size={20} />
+                {isLiked ? <GoHeartFill size={20} /> : <GoHeart size={20} />}
                 {reply.numLikes}
               </button>
             </div>

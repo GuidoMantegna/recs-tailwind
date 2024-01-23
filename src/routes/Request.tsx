fba2ba45db17bf54a5649e1b31da0f9f41962628
Guido Mantegna <gmantegna@elcallao.com>
2024-01-23 16:39:01 -0300

    Create modal component

--- src/routes/Request.tsx
+++ src/routes/Request.tsx
@@ -2,8 +2,14 @@ import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import AskForm from 'components/AskForm'
 import Ask from 'components/Ask'
+import Modal from 'components/Modal'
 import useRequests from '../customHooks/useRequests'
-import { GoCommentDiscussion, GoQuestion, GoHeart, GoHeartFill } from 'react-icons/go'
+import {
+  GoCommentDiscussion,
+  GoQuestion,
+  GoHeart,
+  GoHeartFill
+} from 'react-icons/go'
 import { getFormattedDate } from 'utils'
 import { useContext } from 'react'
 import { AuthContext } from 'context'
@@ -12,6 +18,7 @@ import axios from 'axios'
 const Request: React.FC = () => {
   const { id } = useParams<{ id: string }>()
   const { asks, loading, error } = useRequests(id)
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const user = useContext(AuthContext)
   // const [isLiked, setIsLiked] = useState(false)
   const handleLike = async (replyID: string) => {
@@ -23,13 +30,18 @@ const Request: React.FC = () => {
       console.log(error)
     }
   }
-  const isLiked = asks?.[0].replies[0].likes.some((like) => like._id === user?._id)
+  const isLiked = asks?.[0].replies[0].likes.some(
+    (like) => like._id === user?._id
+  )
   return (
     <>
       {loading || !asks ? (
         <p>Loading...</p>
       ) : (
         <>
+          <Modal onClose={() => setIsModalOpen(!isModalOpen)} isOpen={isModalOpen}>
+            <p>Holiiss</p>
+          </Modal>
           <div className="w-full mb-6">
             <div className="flex items-center gap-2">
               <img
@@ -38,32 +50,46 @@ const Request: React.FC = () => {
                 className="rounded-full w-10"
               />
               <p className="">{asks[0].user.name}</p>
-              <span className="font-extralight text-sm">{getFormattedDate(asks[0].createdAt)}</span>
+              <span className="font-extralight text-sm">
+                {getFormattedDate(asks[0].createdAt)}
+              </span>
             </div>
-            <div className="border p-2 h-20 overflow-hidden whitespace-nowrap text-ellipsis dialog-box my-2">
+            <div className="p-4 h-20 overflow-hidden whitespace-nowrap text-ellipsis dialog-box my-2 bg-slate-100">
               {asks[0].brief}
+            </div>
+            <div className="flex items-center gap-2 pl-4">
+              <GoCommentDiscussion size={20} />
+              {`${asks[0].replies.length} ${
+                asks[0].replies.length > 1 ? 'Replies' : 'Reply'
+              }`}
+            </div>
+            <div className='w-full flex justify-center'>
+              <button className='custom-btn w-40 my-4' onClick={() => setIsModalOpen(!isModalOpen)}>Reply</button>
             </div>
           </div>
           {asks[0].replies.map((reply) => (
             <div className="w-full" key={reply._id}>
-            <div className="flex items-center justify-end gap-2">
-              <p className="">{reply.user.name}</p>
-              <img
-                src="/img/users/user-7.jpg"
-                alt="user photo"
-                className="rounded-full w-10"
-              />
+              <div className="flex items-center justify-end gap-2">
+                <p className="">{reply.user.name}</p>
+                <img
+                  src="/img/users/user-7.jpg"
+                  alt="user photo"
+                  className="rounded-full w-10"
+                />
+              </div>
+              <div className="border p-4 h-20 overflow-hidden whitespace-nowrap text-ellipsis dialog-box-right my-2">
+                {reply.reply}
+              </div>
+              <div className="flex justify-end items-center gap-2 text-sm pr-4">
+                <button
+                  className="flex gap-1"
+                  onClick={() => handleLike(reply._id)}
+                >
+                  {isLiked ? <GoHeartFill size={20} /> : <GoHeart size={20} />}
+                  {reply.numLikes}
+                </button>
+              </div>
             </div>
-            <div className="border p-2 h-20 overflow-hidden whitespace-nowrap text-ellipsis dialog-box-right my-2">
-              {reply.reply}
-            </div>
-            <div className="flex justify-end items-center gap-2 text-sm">
-              <button className='flex gap-1 pr-2' onClick={() => handleLike(reply._id)}>
-                {isLiked ? <GoHeartFill size={20} /> : <GoHeart size={20} />}
-                {reply.numLikes}
-              </button>
-            </div>
-          </div>
           ))}
         </>
       )}

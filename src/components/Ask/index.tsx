bb327eedc84745ae0754550682e17d9235109cb5
Guido Mantegna <gmantegna@elcallao.com>
2024-02-09 07:57:33 -0300

    Add a loader for requests

--- src/components/Ask/index.tsx
+++ src/components/Ask/index.tsx
@@ -3,6 +3,24 @@ import { AskProps } from 'interfaces'
 import { GoCommentDiscussion } from 'react-icons/go'
 import { Link } from 'react-router-dom'
 
+const AskLoading = () => {
+  return (
+    <div role="status" className="animate-pulse">
+      <div className="w-full my-8">
+        <div className="flex items-center gap-2">
+          <div className="rounded-full w-10 bg-gray-200 dark:bg-gray-500 h-10"></div>
+          <p className="bg-gray-200 dark:bg-gray-500 w-16 h-4"></p>
+          <span className="bg-gray-200 dark:bg-gray-500 w-14 h-4"></span>
+        </div>
+        <div className="bg-gray-200 dark:bg-gray-500 h-20 dialog-box my-2"></div>
+        <div className="flex justify-end items-center gap-2">
+          <div className="bg-gray-200 dark:bg-gray-500 w-24 h-6"></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 const Ask: React.FC<AskProps> = ({ _id, user, brief, replies, createdAt }) => {
   const date = new Date(createdAt)
   const formattedDate = date
@@ -12,29 +30,33 @@ const Ask: React.FC<AskProps> = ({ _id, user, brief, replies, createdAt }) => {
     })
     .toLocaleUpperCase()
 
-  return (
-    <div className="w-full my-8">
-      <div className="flex items-center gap-2">
-        <img
-          src={`${STATIC_BASE_URL}/img/users/user-${user._id}.jpeg`}
-          alt="user photo"
-          className="rounded-full w-10"
-        />
-        <p className="">{user.name}</p>
-        <span className="font-extralight text-sm">{formattedDate}</span>
-      </div>
-      <div className="border p-2 h-20 overflow-hidden whitespace-nowrap text-ellipsis dialog-box my-2">
-        {brief}
-      </div>
-      <Link
-        to={`/requests/${_id}`}
-        className="flex justify-end items-center gap-2"
-      >
-        <GoCommentDiscussion size={20} />
-        Replies {`${replies.length}`}
-      </Link>
-    </div>
-  )
+  if (true)
+    return (
+      <>
+        {/* <AskLoading /> */}
+        <div className="w-full my-8">
+          <div className="flex items-center gap-2">
+            <img
+              src={`${STATIC_BASE_URL}/img/users/user-${user._id}.jpeg`}
+              alt="user photo"
+              className="rounded-full w-10"
+            />
+            <p className="">{user.name}</p>
+            <span className="font-extralight text-sm">{formattedDate}</span>
+          </div>
+          <div className="border p-2 h-20 overflow-hidden whitespace-nowrap text-ellipsis dialog-box my-2">
+            {brief}
+          </div>
+          <Link
+            to={`/requests/${_id}`}
+            className="flex justify-end items-center gap-2"
+          >
+            <GoCommentDiscussion size={20} />
+            Replies {`${replies.length}`}
+          </Link>
+        </div>
+      </>
+    )
 }
 
 export default Ask
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -7,6 +7,24 @@ import { AskForm, Ask, Error, Loading } from 'components'
 import { useAsks, useFetch } from 'customHooks'
 import { Link } from 'react-router-dom'
 
+const AskLoading = () => {
+  return (
+    <div role="status" className="animate-pulse">
+      <div className="w-full my-8">
+        <div className="flex items-center gap-2">
+          <div className="rounded-full w-10 bg-gray-200 dark:bg-gray-500 h-10"></div>
+          <p className="bg-gray-200 dark:bg-gray-500 w-16 h-4"></p>
+          <span className="bg-gray-200 dark:bg-gray-500 w-14 h-4"></span>
+        </div>
+        <div className="bg-gray-200 dark:bg-gray-500 h-20 dialog-box my-2"></div>
+        <div className="flex justify-end items-center gap-2">
+          <div className="bg-gray-200 dark:bg-gray-500 w-24 h-6"></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 const Home: React.FC = () => {
   const user = useContext(AuthContext)
   const { asks, loading, error, setAsks } = useAsks(undefined, 'allRequests')
@@ -14,12 +32,19 @@ const Home: React.FC = () => {
 
   const makeRequest = (ask: string) => {
     fetchData('requests', 'post', { brief: ask }).then(
-      (data) => asks && data && setAsks([data.request, ...asks])
+      (data) =>
+        asks &&
+        data &&
+        // set asks with current user data beacuse the user is not fetched from the server completely 
+        setAsks([
+          { ...data.request, user: { _id: user?._id, name: user?.name } },
+          ...asks
+        ])
     )
   }
 
   if (error || errorData) return <Error />
-  if (loading || loadingData) return <Loading />
+  // if (loading || loadingData) return <Loading />
   return (
     <>
       {user ? (
@@ -31,6 +56,13 @@ const Home: React.FC = () => {
       )}
 
       <div className="w-full">
+        {loading ||
+          (loadingData && (
+            <>
+              <AskLoading />
+              {asks?.map((ask) => <Ask key={ask._id} {...ask} />)}
+            </>
+          ))}
         {asks?.map((ask) => <Ask key={ask._id} {...ask} />)}
       </div>
     </>
